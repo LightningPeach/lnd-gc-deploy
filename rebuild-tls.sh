a11f69c2bc12956837c9971dde7fb09afad375e8
Vladyslav Budichenko <vladyslav.budichenko@bitfury.com>
2018-10-02 15:14:45 +0300

    Fix rebuild-tls

--- rebuild-tls.sh
+++ rebuild-tls.sh
@@ -2,6 +2,6 @@
 
 IP=$(kubectl get services | grep lnd-pod | awk '{print $4}')
 echo $IP
-sed -i "s/command: \[\".\/start-lnd.sh.*/command: \[\".\/start-lnd.sh\", \"--tlsextradomain=$IP\"\]/g" pv-pod.yml
+sed -i "s/command: \[\".\/start-lnd.sh.*/command: \[\".\/start-lnd.sh\", \"--tlsextradomain=$IP\"\]/g" lnd-pod.yml
 kubectl exec lnd-pod -- rm /root/.lnd/tls*
 kubectl delete pod lnd-pod
