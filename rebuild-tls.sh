cacfdd1ddd7061e1e18c39e12ec041e696f5b4ec
Vladyslav Budichenko <vladyslav.budichenko@bitfury.com>
2018-10-29 13:00:05 +0200

    Change from tlsextradomain to tlsextraip

--- rebuild-tls.sh
+++ rebuild-tls.sh
@@ -2,7 +2,7 @@
 
 IP=$(kubectl get services | grep lnd-pod | awk '{print $4}')
 echo $IP
-sed -i "s/command: \[\".\/start-lnd.sh.*/command: \[\".\/start-lnd.sh\", \"--tlsextradomain=$IP\"\]/g" lnd-pod.yml
+sed -i "s/command: \[\".\/start-lnd.sh.*/command: \[\".\/start-lnd.sh\", \"--tlsextraip=$IP\"\]/g" lnd-pod.yml
 kubectl exec lnd-pod -- rm /root/.lnd/tls.cert
 kubectl exec lnd-pod -- rm /root/.lnd/tls.key
 kubectl delete pod lnd-pod
