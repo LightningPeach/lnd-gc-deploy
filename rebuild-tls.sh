d97bb84c67470d0c02e0d1094358a0776921d2ca
Vladyslav Budichenko <vladyslav.budichenko@bitfury.com>
2018-11-01 17:48:46 +0200

    Fix script

--- rebuild-tls.sh
+++ rebuild-tls.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-
 IP=$(kubectl get services | grep lnd-pod | awk '{print $4}')
-# echo $IP
-# sed -i "s/command: \[\".\/start-lnd.sh.*/command: \[\".\/start-lnd.sh\", \"--tlsextraip=$IP\"\]/g" lnd-pod.yml
 kubectl exec lnd-pod -- rm /root/.lnd/tls.cert
 kubectl exec lnd-pod -- rm /root/.lnd/tls.key
-kubectl exec lnd-pod -- sh -c 'sed -i "s/address/\"$IP\",/g" /server.json'
-# sed -i "s/address/\"$IP\",/g" tmp.json
-# DATA=$(cat tmp.json)
-# kubectl exec lnd-pod -- $DATA >> server.json
-kubectl exec lnd-pod -- cfssl gencert -initca server.json | cfssljson -bare ca -
-kubectl exec lnd-pod -- cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=config.json server.json | cfssljson -bare server
-kubectl exec lnd-pod -- mv /root/.lnd/server.pem /root/.lnd/tls.cert
-kubectl exec lnd-pod -- mv /root/.lnd/server-key.pem /root/.lnd/tls.key
+kubectl exec lnd-pod -- cat server.json > tmp.json
+sed -i "s/address/$IP/g" tmp.json
+kubectl cp tmp.json lnd-pod:/server.new.json
+rm tmp.json
+kubectl exec lnd-pod -- sh -c 'cfssl gencert -initca server.new.json | cfssljson -bare ca -'
+kubectl exec lnd-pod -- sh -c 'cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=config.json server.new.json | cfssljson -bare server'
+kubectl exec lnd-pod -- sh -c 'mv /server.pem /root/.lnd/tls.cert'
+kubectl exec lnd-pod -- sh -c 'mv /server-key.pem /root/.lnd/tls.key'
 kubectl delete pod lnd-pod
