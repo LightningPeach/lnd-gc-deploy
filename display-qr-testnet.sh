6e470000bae2fb1c0e0b36a6c39129d90795994a
Eugene B <gamergektor@gmail.com>
2019-07-18 15:52:03 +0300

    Make xxd oneline output for display qr

--- display-qr-testnet.sh
+++ display-qr-testnet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-(rm qr.txt 2> /dev/null || true) && echo "https://$(kubectl get services | grep lnd-pod | awk '{print $4}'):8080" >> qr.txt && kubectl exec lnd-pod -- xxd -p -c 250 /root/.lnd/data/chain/bitcoin/testnet/admin.macaroon >> qr.txt >> qr.txt && kubectl exec lnd-pod -- cat /root/.lnd/tls.cert >> qr.txt && /usr/bin/qrencode -s 1 -m 2 -t ANSIUTF8 < qr.txt
+(rm qr.txt 2> /dev/null || true) && echo "https://$(kubectl get services | grep lnd-pod | awk '{print $4}'):8080" >> qr.txt && kubectl exec lnd-pod -- xxd -p -c 250 /root/.lnd/data/chain/bitcoin/testnet/admin.macaroon | tr -d '\n' >> qr.txt >> qr.txt && sed -i -e '$a\' qr.txt && kubectl exec lnd-pod -- cat /root/.lnd/tls.cert >> qr.txt && /usr/bin/qrencode -s 1 -m 2 -t ANSIUTF8 < qr.txt
